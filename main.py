f0c4472ed79c35707b5d8767254ac04d2386a9b3
Zekai "Zk" Zhang <94256529+KaiserZZK@users.noreply.github.com>
2025-02-09 14:45:59 -0500

    player basics

--- main.py
+++ main.py
@@ -84,8 +84,55 @@ world_data = [
 ]
 
 
+class Player():
+	def __init__(self, x, y):
+		img = pygame.image.load('assets/img/Player/stein_red.png')
+		self.image = pygame.transform.scale(img, (80, 80))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.vel_y = 0
+		self.jumped = False
+
+	def update(self):
+		dx = 0
+		dy = 0
+
+		#get keypresses
+		key = pygame.key.get_pressed()
+		if key[pygame.K_SPACE] and self.jumped == False:
+			self.vel_y = -15
+			self.jumped = True
+		if key[pygame.K_SPACE] == False:
+			self.jumped = False
+		if key[pygame.K_LEFT]:
+			dx -= 5
+		if key[pygame.K_RIGHT]:
+			dx += 5
+
+
+		#add gravity
+		self.vel_y += 1
+		if self.vel_y > 10:
+			self.vel_y = 10
+		dy += self.vel_y
+
+		#check for collision
+
+		#update player coordinates
+		self.rect.x += dx
+		self.rect.y += dy
+
+		if self.rect.bottom > screen_height:
+			self.rect.bottom = screen_height
+			dy = 0
+
+		#draw player onto screen
+		screen.blit(self.image, self.rect)
+
+
 # Main game loop   
- 
+player = Player(100, screen_height - 130)
 world = World(world_data)
 
 run = True 
@@ -97,6 +144,7 @@ while run:
             screen.blit(bg_img, (x, y))
             
     world.draw()
+    player.update()
             
     draw_grid()
     
