f8bb2926033b4acb681d9dd97250558c5e1a96af
Zekai "Zk" Zhang <94256529+KaiserZZK@users.noreply.github.com>
2025-02-09 15:41:49 -0500

    collision

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ class Player():
 		self.counter = 0
 		for num in range(1, 4):
 			img_right = pygame.image.load(f'assets/img/Player/stein_red_{num}.png')
-			img_right = pygame.transform.scale(img_right, (100, 100))
+			img_right = pygame.transform.scale(img_right, (40, 80))
 			img_left = pygame.transform.flip(img_right, True, False)
 			self.images_right.append(img_right)
 			self.images_left.append(img_left)
@@ -103,6 +103,8 @@ class Player():
 		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
 		self.vel_y = 0
 		self.jumped = False
 		self.direction = 0
@@ -155,6 +157,20 @@ class Player():
 		dy += self.vel_y
 
 		#check for collision
+		for tile in world.tile_list:
+			#check for collision in x direction
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				dx = 0
+			#check for collision in y direction
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				#check if below the ground i.e. jumping
+				if self.vel_y < 0:
+					dy = tile[1].bottom - self.rect.top
+					self.vel_y = 0
+				#check if above the ground i.e. falling
+				elif self.vel_y >= 0:
+					dy = tile[1].top - self.rect.bottom
+					self.vel_y = 0
 
 		#update player coordinates
 		self.rect.x += dx
