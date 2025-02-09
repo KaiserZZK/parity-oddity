61cae6c66ff38d80bec2cbc5b4e913b72e3cb91f
Zekai "Zk" Zhang <94256529+KaiserZZK@users.noreply.github.com>
2025-02-09 14:13:11 -0500

    world

---
+++ main.py
@@ -0,0 +1,110 @@
+import pygame 
+from pygame.locals import * 
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Untitled Gem Platformer')
+
+# Define game vairables 
+tile_size = 50
+
+# Load assets
+bg_img = pygame.image.load("assets/img/Background/Gray.png")
+bg_tile_height = bg_img.get_height()
+bg_tile_width = bg_img.get_width()
+
+# Helpers
+def draw_grid():
+    assert screen_height == screen_width 
+    
+    for line in range(0, screen_width//tile_size+1):
+        pygame.draw.line(screen, (255,255,255), (0, line*tile_size), (screen_width, line*tile_size))
+        pygame.draw.line(screen, (255,255,255), (line*tile_size, 0), (line*tile_size, screen_height))\
+
+class World():
+	def __init__(self, data):
+		self.tile_list = []
+
+		#load images
+		dirt_img = pygame.image.load('assets/img/Terrain/dirt.png')
+		grass_img = pygame.image.load('assets/img/Terrain/grass.png')
+
+		row_count = 0
+		for row in data:
+			col_count = 0
+			for tile in row:
+				if tile == 1:
+					img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * tile_size
+					img_rect.y = row_count * tile_size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				if tile == 2:
+					img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * tile_size
+					img_rect.y = row_count * tile_size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				col_count += 1
+			row_count += 1
+
+	def draw(self):
+		for tile in self.tile_list:
+			screen.blit(tile[0], tile[1])
+
+
+
+world_data = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+    [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+    [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+    [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+    [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+    [1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+    [1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
+    [1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+    [1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+    [1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+    [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+
+# Main game loop   
+ 
+world = World(world_data)
+
+run = True 
+
+while run:
+    
+    for y in range(0, screen_height, bg_tile_height):
+        for x in range(0, screen_width, bg_tile_width): 
+            screen.blit(bg_img, (x, y))
+            
+    world.draw()
+            
+    draw_grid()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    pygame.display.update() 
+            
+
+pygame.quit()
