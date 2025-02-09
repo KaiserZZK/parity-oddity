309972e21b1f1c336bd1355254e5090e5ffda57e
Zekai "Zk" Zhang <94256529+KaiserZZK@users.noreply.github.com>
2025-02-09 18:04:16 -0500

    map data

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame 
 from pygame.locals import * 
+import pickle 
+from os import path
 
 pygame.init()
 
@@ -16,6 +18,8 @@ pygame.display.set_caption('Untitled Gem Platformer')
 tile_size = 50
 game_over = 0
 main_menu = True
+level = 0
+max_levels = 7
 
 # Load assets
 bg_img = pygame.image.load("assets/img/Background/Gray.png")
@@ -26,6 +30,20 @@ restart_img = pygame.image.load('assets/img/Button/restart_btn.png')
 start_img = pygame.image.load('assets/img/Button/start_btn.png')
 exit_img = pygame.image.load('assets/img/Button/exit_btn.png')
 
+#function to reset level
+def reset_level(level):
+	player.reset(100, screen_height - 130)
+	blob_group.empty()
+	trap_group.empty()
+	exit_group.empty()
+
+	#load in level data and create world
+	if path.exists(f'map/level{level}_data'):
+		pickle_in = open(f'map/level{level}_data', 'rb')
+		world_data = pickle.load(pickle_in)
+	world = World(world_data)
+
+	return world
 
 class Button():
 	def __init__(self, x, y, image):
@@ -88,6 +106,9 @@ class World():
 				if tile == 6:
 					trap = Trap(col_count * tile_size, row_count * tile_size + (tile_size // 2))
 					trap_group.add(trap)
+				if tile == 8:
+					exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+					exit_group.add(exit)
 				col_count += 1
 			row_count += 1
 
@@ -120,55 +141,19 @@ class Trap(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
-
-
-world_data = [
-	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-	[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
-	[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
-	[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
-	[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-	[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
-	[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-	[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
-	[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
-	[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-	[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-	[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-]
-
-
-class Player():
+  
+class Exit(pygame.sprite.Sprite):
 	def __init__(self, x, y):
-		self.images_right = []
-		self.images_left = []
-		self.index = 0
-		self.counter = 0
-		for num in range(1, 4):
-			img_right = pygame.image.load(f'assets/img/Player/stein_red_{num}.png')
-			img_right = pygame.transform.scale(img_right, (40, 80))
-			img_left = pygame.transform.flip(img_right, True, False)
-			self.images_right.append(img_right)
-			self.images_left.append(img_left)
-		self.image = self.images_right[self.index]
-		self.dead_image = pygame.image.load('assets/img/Player/ghost.png')
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('assets/img/Terrain/exit.png')
+		self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
 		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
-		self.width = self.image.get_width()
-		self.height = self.image.get_height()
-		self.vel_y = 0
-		self.jumped = False
-		self.direction = 0
-		self.in_air = True
+
+class Player():
+	def __init__(self, x, y):
+		self.reset(x, y)
 
 	def update(self, game_over):
 		dx = 0
@@ -243,6 +228,10 @@ class Player():
 				#check for collision with trap
 				if pygame.sprite.spritecollide(self, trap_group, False):
 					game_over = -1
+     
+				#check for collision with exit
+				if pygame.sprite.spritecollide(self, exit_group, False):
+					game_over = 1
 
 			#update player coordinates
 			self.rect.x += dx
@@ -285,6 +274,13 @@ class Player():
 player = Player(100, screen_height - 130)
 blob_group = pygame.sprite.Group()
 trap_group = pygame.sprite.Group()
+exit_group = pygame.sprite.Group()
+
+#load in level data and create world
+if path.exists(f'map/level{level}_data'):
+	pickle_in = open(f'map/level{level}_data', 'rb')
+	world_data = pickle.load(pickle_in)
+
 world = World(world_data)
 
 restart_button = Button(screen_width // 2 - 50, screen_height // 2 + 100, restart_img)
@@ -313,12 +309,34 @@ while run:
 		
 		blob_group.draw(screen)
 		trap_group.draw(screen)
+		exit_group.draw(screen)
 
 		game_over = player.update(game_over)
+  
+		# Lose condition met
 		if game_over == -1:
 			if restart_button.draw():
-				player.reset(100, screen_height - 130)
+				world_data = []
+				world = reset_level(level)
 				game_over = 0
+
+		# Level advance condition met  
+		if game_over == 1:
+			#reset game and go to next level
+			level += 1
+			if level <= max_levels:
+				#reset level
+				world_data = []
+				world = reset_level(level)
+				game_over = 0
+			else:
+				if restart_button.draw():
+					level = 1
+					#reset level
+					world_data = []
+					world = reset_level(level)
+					game_over = 0
+
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
--- utils/level_editor.py
+++ utils/level_editor.py
@@ -162,8 +162,8 @@ while run:
 		pickle_out.close()
 	if load_button.draw():
 		#load in level data
-		if path.exists(f'level{level}_data'):
-			pickle_in = open(f'level{level}_data', 'rb')
+		if path.exists(f'map/level{level}_data'):
+			pickle_in = open(f'map/level{level}_data', 'rb')
 			world_data = pickle.load(pickle_in)
 
 
