cf97dd015555231b41d7c45f5a92055f9971d536
Zekai "Zk" Zhang <94256529+KaiserZZK@users.noreply.github.com>
2025-02-09 15:59:24 -0500

    enemy actions

--- main.py
+++ main.py
@@ -54,6 +54,9 @@ class World():
 					img_rect.y = row_count * tile_size
 					tile = (img, img_rect)
 					self.tile_list.append(tile)
+				if tile == 3:
+					blob = Enemy(col_count * tile_size, row_count * tile_size + 15)
+					blob_group.add(blob)        
 				col_count += 1
 			row_count += 1
 
@@ -61,7 +64,22 @@ class World():
 		for tile in self.tile_list:
 			screen.blit(tile[0], tile[1])
 
+class Enemy(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load('assets/img/Enemy/blob.png')
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.move_direction = 1
+		self.move_counter = 0
 
+	def update(self):
+		self.rect.x += self.move_direction
+		self.move_counter += 1
+		if abs(self.move_counter) > 50:
+			self.move_direction *= -1
+			self.move_counter *= -1
 
 world_data = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
@@ -186,6 +204,7 @@ class Player():
 
 # Main game loop   
 player = Player(100, screen_height - 130)
+blob_group = pygame.sprite.Group()
 world = World(world_data)
 
 run = True 
@@ -199,6 +218,8 @@ while run:
             screen.blit(bg_img, (x, y))
             
     world.draw()
+    blob_group.update()
+    blob_group.draw(screen)
     player.update()
             
     draw_grid()
