a19d15ba0762c7f3d29e7ff0f50a4f0414a96065
Zekai "Zk" Zhang <94256529+KaiserZZK@users.noreply.github.com>
2025-02-10 23:32:57 -0500

    size adjustment

--- utils/level_editor.py
+++ utils/level_editor.py
@@ -46,19 +46,6 @@ green = (144, 201, 120)
 
 font = pygame.font.SysFont('Futura', 24)
 
-#create empty tile list
-world_data = []
-for row in range(100):
-	r = [0] * 100
-	world_data.append(r)
-
-#create boundary
-for tile in range(0, 100):
-	world_data[19][tile] = 2
-	# world_data[0][tile] = 1
-	# world_data[tile][0] = 1
-	# world_data[tile][19] = 1
-
 #function for outputting text onto the screen
 def draw_text(text, font, text_col, x, y):
 	img = font.render(text, True, text_col)
@@ -72,9 +59,11 @@ def draw_grid():
 		pygame.draw.line(screen, white, (0, c * tile_size), (screen_width, c * tile_size))
 
 
-def draw_world():
-	for row in range(cols):
-		for col in range(cols):
+def draw_world(map_height, map_width):
+	map_rows = map_height // tile_size
+	map_cols = map_width // tile_size 
+	for row in range(map_rows):
+		for col in range(map_cols):
 			if world_data[row][col] > 0:
 				if world_data[row][col] == 1:
 					#dirt blocks
@@ -142,6 +131,48 @@ class Button():
 save_button = Button(screen_width // 2 - 150, screen_height - 80, save_img)
 load_button = Button(screen_width // 2 + 50, screen_height - 80, load_img)
 
+# decouple map and screen sizes
+map_height, map_width = 400, 300 # screen_height, screen_width 
+input_rect = pygame.Rect(200, 200, 140, 32)
+text = "" 
+
+#create empty tile list
+world_data = []
+for row in range(map_height // tile_size):
+	r = [0] * (map_width // tile_size )
+	world_data.append(r)
+
+def expand_world_data(world_data, lx, rx, ly, ry):
+	lx //= tile_size
+	rx //= tile_size 
+	ly //= tile_size 
+	ry //= tile_size 
+	# Pad height
+	world_data = [[0]*len(world_data[0])]*ly + world_data + [[0]*len(world_data[0])]*ry
+	# Pad width 
+	for i in range(len(world_data)):
+		world_data[i] = [0]*lx + world_data[i] + [0]*rx
+	return world_data
+
+def shrink_world_data(world_data, lx, rx, ly, ry):
+	lx //= tile_size
+	rx //= tile_size 
+	ly //= tile_size 
+	ry //= tile_size 
+	# Pad height 
+	if ry < 0:
+		world_data = world_data[abs(ly):ry]
+	else: 
+		world_data = world_data[abs(ly):]
+	# Pad width 
+	for i in range(len(world_data)):
+		if rx < 0:
+			world_data[i] = world_data[i][abs(lx):rx]
+		else:
+			world_data[i] = world_data[i][abs(lx):]
+	return world_data
+
+
 #main game loop
 run = True
 while run:
@@ -150,8 +181,8 @@ while run:
 
 	#draw background
 	screen.fill(green)
-	for y in range(0, screen_height, bg_tile_height):
-		for x in range(0, screen_width, bg_tile_width): 
+	for y in range(0, map_height, bg_tile_height):
+		for x in range(0, map_width, bg_tile_width): 
 			screen.blit(bg_img, (x, y))
 
 	#load and save level
@@ -165,16 +196,18 @@ while run:
 		if path.exists(f'level{level}_data'):
 			pickle_in = open(f'level{level}_data', 'rb')
 			world_data = pickle.load(pickle_in)
-
+			map_height = len(world_data) * tile_size
+			map_width = len(world_data[0]) * tile_size
 
 	#show the grid and draw the level tiles
 	draw_grid()
-	draw_world()
+	draw_world(map_height=map_height, map_width=map_width)
 
 
 	#text showing current level
-	draw_text(f'Level: {level}', font, white, tile_size, screen_height - 60)
-	draw_text('Press UP or DOWN to change level', font, white, tile_size, screen_height - 40)
+	draw_text(f'Current modification data input {text}', font, white, tile_size, screen_height - 80)
+	draw_text(f'Height {map_height} Width {map_width} tile size {tile_size}', font, white, tile_size, screen_height - 60)
+	draw_text('{level} Press UP or DOWN to change level', font, white, tile_size, screen_height - 40)
 
 	#event handler
 	for event in pygame.event.get():
@@ -183,12 +216,12 @@ while run:
 			run = False
 		#mouseclicks to change tiles
 		if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
-			clicked = True
+			clicked = True 
 			pos = pygame.mouse.get_pos()
 			x = pos[0] // tile_size
 			y = pos[1] // tile_size
 			#check that the coordinates are within the tile area
-			if x < 100 and y < 100:
+			if x < (map_width//tile_size) and y < (map_height//tile_size):
 				#update tile value
 				if pygame.mouse.get_pressed()[0] == 1:
 					world_data[y][x] += 1
@@ -205,7 +238,24 @@ while run:
 			if event.key == pygame.K_UP:
 				level += 1
 			elif event.key == pygame.K_DOWN and level > 1:
-				level -= 1
+				level -= 1 
+			elif event.key == pygame.K_RETURN:
+				lx, rx, ly, ry = text.split(",")
+				lx, rx, ly, ry = int(lx), int(rx), int(ly), int(ry)
+				if lx<0 or rx<0 or ly<0 or ry<0:
+					world_data = shrink_world_data(world_data, lx, rx, ly, ry)
+				else:
+					world_data = expand_world_data(world_data , lx, rx, ly, ry)
+				map_height = len(world_data) * tile_size
+				map_width = len(world_data[0]) * tile_size
+				text = ""
+	
+			elif event.key == pygame.K_BACKSPACE:
+				text =  text[:-1]
+			else:
+				text += event.unicode
+	
+		pygame.display.flip()
 
 	#update game display window
 	pygame.display.update()
